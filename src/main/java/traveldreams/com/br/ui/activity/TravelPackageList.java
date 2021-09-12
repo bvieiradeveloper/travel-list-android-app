907b1657afef2f3511f4ae65c4c052c27d3dbf42
hzviera <88221181+hzviera@users.noreply.github.com>
2021-09-11 23:21:23 -0300

    first commit

---
+++ src/main/java/traveldreams/com/br/ui/activity/TravelPackageList.java
@@ -0,0 +1,16 @@
+package traveldreams.com.br.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import traveldreams.com.br.R;
+
+public class TravelPackageList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travel_package_list);
+    }
+}
