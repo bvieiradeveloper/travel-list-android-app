4141f82a551703964577e96a93fd28f111bca8a7
hzviera <88221181+hzviera@users.noreply.github.com>
2021-09-13 09:57:09 -0300

    Package Entities

---
+++ src/main/java/traveldreams/com/br/ui/Model/Package.java
@@ -0,0 +1,33 @@
+package traveldreams.com.br.ui.Model;
+
+import java.math.BigDecimal;
+
+public class Package {
+    private final String local;
+    private final String image;
+    private final int days;
+    private final BigDecimal price;
+
+    public Package(String local, String image, int days, BigDecimal price) {
+        this.local = local;
+        this.image = image;
+        this.days = days;
+        this.price = price;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
