e2bc4d3a56fd5d85c80e5565ba665c6ee364f385
hzviera <88221181+hzviera@users.noreply.github.com>
2021-09-13 10:58:51 -0300

    End Layout

--- src/main/java/traveldreams/com/br/ui/activity/TravelPackageList.java
+++ src/main/java/traveldreams/com/br/ui/activity/TravelPackageList.java
@@ -3,8 +3,12 @@ package traveldreams.com.br.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 import traveldreams.com.br.R;
+import traveldreams.com.br.ui.DAO.PackageDAO;
+import traveldreams.com.br.ui.adapter.PackageListOptions;
 
 public class TravelPackageList extends AppCompatActivity {
 
@@ -12,5 +16,9 @@ public class TravelPackageList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_travel_package_list);
+
+        ListView packageList = findViewById(R.id.package_list);
+        PackageDAO packageDAO = new PackageDAO();
+        packageList.setAdapter(new PackageListOptions(packageDAO,this));
     }
 }
---
+++ src/main/java/traveldreams/com/br/ui/adapter/PackageListOptions.java
@@ -0,0 +1,67 @@
+package traveldreams.com.br.ui.adapter;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.math.RoundingMode;
+
+import traveldreams.com.br.R;
+import traveldreams.com.br.ui.DAO.PackageDAO;
+import traveldreams.com.br.ui.activity.TravelPackageList;
+
+public class PackageListOptions extends BaseAdapter {
+
+    Context context;
+    PackageDAO packageDAO;
+    public PackageListOptions(PackageDAO packageDAO, Context context) {
+        this.context = context;
+        this.packageDAO = packageDAO;
+    }
+
+    @Override
+    public int getCount() {
+        return packageDAO.list().size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return packageDAO.list().get(position);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup)
+    {
+        View createdView = LayoutInflater.from(context)
+                .inflate(R.layout.item_package, viewGroup, false);
+
+        TextView local = createdView.findViewById(R.id.item_package_local);
+        local.setText(packageDAO.list().get(position).getLocal());
+
+        TextView days = createdView.findViewById(R.id.item_package_days);
+        days.setText(packageDAO.list().get(position).getDays() + " days");
+
+        TextView price = createdView.findViewById(R.id.item_package_price);
+        price.setText("R$ " + packageDAO.list().get(position).getPrice().setScale(2, RoundingMode.DOWN));
+
+        ImageView image= createdView.findViewById(R.id.item_package_image_local);
+        Resources resources = context.getResources();
+        int imageResourceId = resources.getIdentifier(packageDAO.list().get(position).getImage(), "drawable", context.getPackageName());
+        Drawable imageResource = resources.getDrawable(imageResourceId);
+        image.setImageDrawable(imageResource);
+
+        return createdView;
+    }
+}
