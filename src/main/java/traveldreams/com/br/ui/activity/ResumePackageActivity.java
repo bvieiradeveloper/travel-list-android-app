43f4409a81f50f731fb60858c8f4fcc137c26f50
hzviera <88221181+hzviera@users.noreply.github.com>
2021-09-20 08:51:48 -0300

    Start Resume Layout

---
+++ src/main/java/traveldreams/com/br/ui/activity/ResumePackageActivity.java
@@ -0,0 +1,16 @@
+package traveldreams.com.br.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import traveldreams.com.br.R;
+
+public class ResumePackageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resume_package);
+    }
+}
