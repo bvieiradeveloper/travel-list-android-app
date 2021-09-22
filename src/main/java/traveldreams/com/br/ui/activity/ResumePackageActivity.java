fe5497a78ebe5c90517fea45caaa14e95cfe5c94
hzviera <88221181+hzviera@users.noreply.github.com>
2021-09-22 20:13:14 -0300

    Data de ida e volta

--- src/main/java/traveldreams/com/br/ui/activity/ResumePackageActivity.java
+++ src/main/java/traveldreams/com/br/ui/activity/ResumePackageActivity.java
@@ -9,6 +9,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import traveldreams.com.br.R;
 import traveldreams.com.br.model.Package;
@@ -23,6 +26,8 @@ public class ResumePackageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resume_package);
 
+        setTitle("Package Resume");
+
         final Package aPackage = new Package("São Paulo", "sao_paulo_sp", 2,  new BigDecimal(243.99));
         Drawable drawableResource = ResourceUtil.getDrawableResource(aPackage.getImage(), this);
 
@@ -33,11 +38,22 @@ public class ResumePackageActivity extends AppCompatActivity {
         local.setText(aPackage.getLocal());
 
         String formatedDay = DayUtil.dayFormat(aPackage.getDays());
-        TextView days = findViewById(R.id.resume_package_date);
+        TextView days = findViewById(R.id.resume_package_days);
         days.setText(formatedDay);
 
         String formatedPrice = CoinUtil.coinFormat(aPackage.getPrice());
         TextView price = findViewById(R.id.resume_package_price);
         price.setText(formatedPrice);
+
+        Calendar departureDate  = Calendar.getInstance();
+        Calendar returnDate  = Calendar.getInstance();
+        returnDate.add(returnDate.DATE,aPackage.getDays());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM");
+
+        String RoundTrip = dateFormat.format(departureDate.getTime()) + " - " + dateFormat.format(returnDate.getTime()) + " at " + returnDate.get(Calendar.YEAR);
+
+        TextView tripDuration = findViewById(R.id.resume_package_date);
+        tripDuration.setText(RoundTrip);
+
     }
 }
